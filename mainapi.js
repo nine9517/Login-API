dd7c361d024e114fdb1724645be90c176cb37004
TheNineInwKung <nine4567@gmail.com>
2018-11-02 03:55:13 +0700

    fix connect mongodb

--- mainapi.js
+++ mainapi.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = process.env.PORT || 55555;
 const mongoose = require('mongoose');
 
@@ -9,13 +9,15 @@ require('./helpers/mongohelper')(mongoose);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.options('*', cors());
 
 app.use('/api/users', require('./routes/users'));
 app.get('/',(req,res)=>{
     res.status(200).send("Welcome to Login API");
 });
 
- var server = http.listen(port, function() {
+app.listen(port, function() {
     console.log('Login-API Server is running on port '+port+' at '+new Date());
 });
 
