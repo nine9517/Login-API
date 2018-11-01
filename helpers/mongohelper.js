efd908bdc1271c9dab19526c0913665ac82b18b0
TheNineInwKung <nine4567@gmail.com>
2018-11-02 00:39:11 +0700

    fix connect mongodb

--- helpers/mongohelper.js
+++ helpers/mongohelper.js
@@ -1,5 +1,5 @@
 const dbConfig = require('../configuration/mongoDB');
 const mongoose = require('mongoose');
 module.exports = ()=>{
-    mongoose.connect('mongodb://'+dbConfig.DB_USERNAME+':'+dbConfig.DB_PASSWORD+'@'+dbConfig.DB_PORT+':'+dbConfig.DB_NAME+'/'+dbConfig.DB_NAME, { useNewUrlParser: true });
+    mongoose.connect('mongodb://'+dbConfig.DB_USERNAME+':'+dbConfig.DB_PASSWORD+'@'+dbConfig.DB_HOST+':'+dbConfig.DB_PORT+'/'+dbConfig.DB_NAME, { useNewUrlParser: true });
 };
