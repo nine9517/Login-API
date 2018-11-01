22fb1d2903e0029af7e9e27c25c65a9b35c21728
TheNineInwKung <nine4567@gmail.com>
2018-11-02 03:53:40 +0700

    change user Schema

--- mainapi.js
+++ mainapi.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 const http = require('http').Server(app);
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const port = process.env.PORT || 55555;
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://'+dbConfig.DB_USERNAME+':'+dbConfig.DB_PASSWORD+'@'+dbConfig.DB_HOST+':'+dbConfig.DB_PORT+'/'+dbConfig.DB_NAME, { useNewUrlParser: true });
 require('./helpers/mongohelper')(mongoose);
 
-app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.options('*', cors());
 
 app.use('/api/users', require('./routes/users'));
 app.get('/',(req,res)=>{
